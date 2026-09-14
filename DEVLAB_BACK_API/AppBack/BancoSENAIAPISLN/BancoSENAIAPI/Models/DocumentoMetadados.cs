08dd6b10062288acafd333c06b829e759b0f2c31
cauadrocha-dot <caua.d.rocha@aluno.senai.br>
2026-09-14 07:27:52 -0300

    Update DocumentoMetadados.cs

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadados.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadados.cs
@@ -1,6 +1,12 @@
 ﻿namespace BancoSENAIAPI.Models
 {
-    public class Class
+    public class DocumentoMetadados
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Extensao { get; set; }
+        public string Caminho { get; set; }
+        public int CodigoCliente { get; set; }
+
     }
 }
